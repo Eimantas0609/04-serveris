cf9f684b55bdcc5f1987f2fd2ed7f39d646f50df
Eimantas0609 <100865215+Eimantas0609@users.noreply.github.com>
2022-07-12 17:47:16 +0300

    to cookies

--- api/account.js
+++ api/account.js
@@ -1,28 +1,27 @@
+import { file } from "../lib/file.js";
 import { IsValid } from "../lib/is-valid/IsValid.js";
+import { utils } from "../lib/utils.js";
 
 const handler = {};
 
-handler.account = (data, res) => {
+handler.account = async (data, callback) => {
     // kliento intensija - ka jis nori daryti?
     const acceptableMethods = ['get', 'post', 'put', 'delete'];
 
     if (acceptableMethods.includes(data.httpMethod)) {
         const httpMethodFunc = handler._innerMethods[data.httpMethod];
-        return httpMethodFunc(data, res);
+        return await httpMethodFunc(data, callback);
     }
 
-    return res.end(JSON.stringify('Tavo norimas HTTPmethod yra nepalaikomas'));
+    return callback(405, {
+        msg: 'Tavo norimas HTTPmethod yra nepalaikomas',
+    });
 }
 
 handler._innerMethods = {};
 
-// GET
-handler._innerMethods.get = (data, res) => {
-    return res.end(JSON.stringify('Account: get'));
-}
-
 // POST - sukuriame paskyra
-handler._innerMethods.post = (data, res) => {
+handler._innerMethods.post = async (data, callback) => {
     const { payload } = data;
 
     /*
@@ -33,26 +32,37 @@ handler._innerMethods.post = (data, res) => {
         - isitikinti, jog atejusiame objekte nera kitu key's apart: email, fullname ir password
     */
 
-    const allowedKeys = ['fullname', 'email', 'pass'];
-    if (Object.keys(payload).length > allowedKeys.length) {
-        return res.end(JSON.stringify('Atsiustuose duomenyse gali buti tik: fullname, email ir pass'));
+    const [validErr, validMsg] = utils.objectValidator(payload, {
+        required: ['fullname', 'email', 'pass'],
+    });
+
+    if (validErr) {
+        return callback(400, {
+            msg: validMsg,
+        });
     }
 
     const { fullname, email, pass } = payload;
 
     const [fullnameErr, fullnameMsg] = IsValid.fullname(fullname);
     if (fullnameErr) {
-        return res.end(JSON.stringify(fullnameMsg));
+        return callback(400, {
+            msg: fullnameMsg,
+        });
     }
 
     const [emailErr, emailMsg] = IsValid.email(email);
     if (emailErr) {
-        return res.end(JSON.stringify(emailMsg));
+        return callback(400, {
+            msg: emailMsg,
+        });
     }
 
     const [passErr, passMsg] = IsValid.password(pass);
     if (passErr) {
-        return res.end(JSON.stringify(passMsg));
+        return callback(400, {
+            msg: passMsg,
+        });
     }
 
     /*
@@ -61,7 +71,13 @@ handler._innerMethods.post = (data, res) => {
         - jei ne - tęsiam
     */
 
-
+    // accounts/${user-email}.json
+    const [readErr] = await file.read('accounts', email + '.json');
+    if (!readErr) {
+        return callback(400, {
+            msg: 'Paskyra jau egzistuoja',
+        })
+    }
 
     /*
     3) issaugoti duomenis (payload)
@@ -70,19 +86,165 @@ handler._innerMethods.post = (data, res) => {
         - jei nepavyko - error
     */
 
-    console.log(payload);
+    delete payload.pass;
+    payload.hashedPassword = utils.hash(pass)[1];
+    payload.lastLoginDate = 0;
+    payload.registrationData = Date.now();
+    payload.browser = data.user.browser;
 
-    return res.end(JSON.stringify('Paskyra sukurta sekmingai'));
+    const [createErr] = await file.create('accounts', email + '.json', payload);
+    if (createErr) {
+        return callback(500, {
+            msg: 'Nepavyko sukurti paskyrtos del vidines serverio klaidos. Pabandykite veliau',
+        })
+    }
+
+    return callback(200, {
+        msg: 'Paskyra sukurta sekmingai',
+    });
 }
 
-// PUT (kapitalinis info pakeistimas) / PATCH (vienos info dalies pakeitimas)
-handler._innerMethods.put = (data, res) => {
-    return res.end(JSON.stringify('Account: put'));
+// GET
+handler._innerMethods.get = async (data, callback) => {
+
+    // 1) suzinoti apie kuri vartotoja norima gauti duomenis
+    const email = data.searchParams.get('email');
+
+    // 2) Patikriname ar gautas email yra email formato
+    const [emailErr, emailMsg] = IsValid.email(email);
+    if (emailErr) {
+        return callback(400, {
+            msg: emailMsg,
+        });
+    }
+
+    // 3) Bandom perskaityti vartotojo duomenis
+    // - jei ERROR - vartotojas neegzistuoja
+    // - jei OK - vartotojas egzistuoja ir gavom jo duomenis
+    const [readErr, readMsg] = await file.read('accounts', email + '.json');
+    if (readErr) {
+        return callback(404, {
+            msg: 'Toks vartotojas neegzistouja, arba nepavyko gauti duomenu del teisiu trukumo',
+        });
+    }
+
+    const [userErr, userData] = utils.parseJSONtoObject(readMsg);
+    if (userErr) {
+        return callback(500, {
+            msg: 'Nepavyko nuskaityti duomenu',
+        });
+    }
+
+    delete userData.hashedPassword;
+
+    return callback(200, {
+        msg: userData,
+    });
+}
+
+// PUT (kapitalinis info pakeistimas)
+// PATCH (vienos info dalies pakeitimas)
+// Leidziam pasikeisti tik: fullname, pass -> hashedPassword
+handler._innerMethods.put = async (data, callback) => {
+    const { payload } = data;
+    const email = data.searchParams.get('email');
+
+    const [emailErr, emailMsg] = IsValid.email(email);
+    if (emailErr) {
+        return callback(400, {
+            msg: emailMsg,
+        });
+    }
+
+    const [validErr, validMsg] = utils.objectValidator(payload, {
+        optional: ['fullname', 'pass'],
+    });
+
+    if (validErr) {
+        return callback(400, {
+            msg: validMsg,
+        });
+    }
+
+    const { fullname, pass } = payload;
+
+    if (fullname) {
+        const [fullnameErr, fullnameMsg] = IsValid.fullname(fullname);
+        if (fullnameErr) {
+            return callback(400, {
+                msg: fullnameMsg,
+            });
+        }
+    }
+
+    if (pass) {
+        const [passErr, passMsg] = IsValid.password(pass);
+        if (passErr) {
+            return callback(400, {
+                msg: passMsg,
+            });
+        }
+    }
+
+    const [readErr, readMsg] = await file.read('accounts', email + '.json');
+    if (readErr) {
+        return callback(404, {
+            msg: 'Toks vartotojas neegzistouja, arba nepavyko gauti duomenu del teisiu trukumo',
+        });
+    }
+
+    const [parseErr, userData] = utils.parseJSONtoObject(readMsg);
+    if (parseErr) {
+        return callback(500, {
+            msg: 'Nepavyko atnaujinti paskyros informacijos, del vidines serverio klaidos',
+        });
+    }
+
+    if (fullname) {
+        userData.fullname = fullname;
+    }
+    if (pass) {
+        userData.hashedPassword = utils.hash(pass)[1];
+    }
+
+    const [updateErr] = await file.update('accounts', email + '.json', userData);
+
+    if (updateErr) {
+        return callback(500, {
+            msg: 'Nepavyko atnaujinti paskyros informacijos, del vidines serverio klaidos',
+        });
+    }
+
+    return callback(200, {
+        msg: 'Vartotojo informacija sekmingai atnaujinta',
+    });
 }
 
 // DELETE
-handler._innerMethods.delete = (data, res) => {
-    return res.end(JSON.stringify('Account: delete'));
+handler._innerMethods.delete = async (data, callback) => {
+    // 1) suzinoti apie kuri vartotoja norima gauti duomenis
+    const email = data.searchParams.get('email');
+
+    // 2) Patikriname ar gautas email yra email formato
+    const [emailErr, emailMsg] = IsValid.email(email);
+    if (emailErr) {
+        return callback(400, {
+            msg: emailMsg,
+        });
+    }
+
+    // 3) Trinam paskyra
+    const [deleteErr] = await file.delete('accounts', email + '.json', userData);
+
+    if (deleteErr) {
+        return callback(500, {
+            msg: 'Nepavyko istrinti paskyros informacijos, del vidines serverio klaidos',
+        });
+    }
+
+    return callback(200, {
+        msg: 'Paskyra istrinta sekmingai',
+    });
 }
 
 export default handler;
